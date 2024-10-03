fa978f7936785fd3d89f8be3923886032de3a346
Sergey <sergeykachaliukdev@gmail.com>
2024-10-03 14:20:34 +0300

    fix: bug when app reloads after user clicks "Enter" om mobile phone

--- src/components/TodoList/TodoControls/TodoControls.tsx
+++ src/components/TodoList/TodoControls/TodoControls.tsx
@@ -1,10 +1,11 @@
 import SearchBar from "./SearchBar";
 import FiltersDropdown from "./FiltersDropdown";
 import ThemeController from "./ThemeController";
+import { FormEvent } from "react";
 
 export default function TodoControls() {
     return (
-        <form>
+        <form onSubmit={(e: FormEvent) => e.preventDefault()}>
             <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-4">
                 <SearchBar />
                 <FiltersDropdown />
